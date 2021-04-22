092b10b2cce55685c14ace5d7fc207d0391582ec
ketty <ketty1554@163.com>
2021-04-22 16:23:59 +0800

    +)1.第一天的代码设计完善

--- src/main/java/com/mashibing/ketty/Main.java
+++ src/main/java/com/mashibing/ketty/Main.java
@@ -1,10 +1,5 @@
 package com.mashibing.ketty;
 
-import java.awt.*;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-
-
 
 /**
  * @author 孺子韫
@@ -16,28 +11,20 @@ public class Main {
      *
      * @param args
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
 //       new Frame() 一个窗口
-        Frame frame = new Frame();
-//      设置窗口的大小
-        frame.setSize(800, 600);
-//      设置是否可以改变大小
-        frame.setResizable(false);
-//      设置窗口的标题
-        frame.setTitle("Tank War");
-//      调用程序窗口显示
-        frame.setVisible(true);
-//      添加windows的监听器，重写windowClosing方法,监听窗口关闭事件
-        frame.addWindowListener(new WindowAdapter() {
+        TankFrame tankFrame = new TankFrame();
 
-            // 设置可以手动关闭当前的窗口
-            @Override
-            public void windowClosing(WindowEvent e) {
-                //  系统退出
-                System.exit(0);
-            }
-        });
+        while(true){
+
+//          主线程沉睡,相当于50毫秒
+            Thread.sleep(50);
+
+//          画笔不停的调用,刷新画面
+            tankFrame.repaint();
+        }
+
 
     }
 }
---
+++ src/main/java/com/mashibing/ketty/TankFrame.java
@@ -0,0 +1,116 @@
+package com.mashibing.ketty;
+
+import java.awt.*;
+import java.awt.event.*;
+
+
+
+/**
+ * @author 孺子韫
+ * @create 2021-04-22 14:16
+ */
+public class TankFrame extends Frame {
+
+    int x=200,y=200;
+
+    public TankFrame() {
+        //      设置窗口的大小
+        setSize(800, 600);
+//      设置是否可以改变大小
+        setResizable(false);
+//      设置窗口的标题
+        setTitle("Tank War");
+//      调用程序窗口显示
+        setVisible(true);
+
+        this.addKeyListener(new MyKeyListener());
+//      添加windows的监听器，重写windowClosing方法,监听窗口关闭事件
+        addWindowListener(new WindowAdapter() {
+
+            // 设置可以手动关闭当前的窗口
+            @Override
+            public void windowClosing(WindowEvent e) {
+                //  系统退出
+                System.exit(0);
+            }
+        });
+    }
+
+    /**
+     * 创建awt图形界面后系统自动调用paint方法
+     * @param graphics
+     */
+    @Override
+    public void paint(Graphics graphics) {
+//        绘制一个坐标x,y,宽50和高50的矩形
+        graphics.fillRect(x,y,50,50);
+//        x+=10;
+//        y+=10;
+    }
+
+
+    /**
+     * 内部类,键盘监听
+     * 处理键盘事件
+     */
+    class MyKeyListener extends KeyAdapter {
+
+//      设定变量,上下左右
+        boolean bL=false;
+        boolean bU=false;
+        boolean bR=false;
+        boolean bD=false;
+
+
+        /**
+         * Invoked when a key has been pressed.
+         *  键盘按下事件
+         * @param e
+         */
+        @Override
+        public void keyPressed(KeyEvent e) {
+//          判断当前按键的状态,如果按下则进行对呀的方法执行
+            int keyCode = e.getKeyCode();
+            switch (keyCode){
+                case KeyEvent.VK_LEFT:
+                    bL=true;
+                    break;
+                case KeyEvent.VK_UP:
+                    bU=true;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    bR=true;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    bD=true;
+                    break;
+            }
+        }
+
+        /**
+         * Invoked when a key has been released.
+         * 键盘抬起事件
+         * @param e
+         */
+        @Override
+        public void keyReleased(KeyEvent e) {
+//          判断当前按键的状态,如果抬起则进行对呀的方法执行
+            int keyCode = e.getKeyCode();
+            switch (keyCode){
+                case KeyEvent.VK_LEFT:
+                    bL=false;
+                    break;
+                case KeyEvent.VK_UP:
+                    bU=false;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    bR=false;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    bD=false;
+                    break;
+            }
+        }
+    }
+
+}
